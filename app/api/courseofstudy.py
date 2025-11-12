0dca1d2f201aac4c885801e8e44cc08ff0c3c6d9
ConConner <egal3d@gmail.com>
2025-11-12 14:25:09 +0100

    delete instance

--- app/api/courseofstudy.py
+++ app/api/courseofstudy.py
@@ -1,14 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.core.db import get_db
-from app.schemas.coursesofstudy import (
-  CourseofStudyCreate,
-  CourseofStudyRead
-)
+from app.schemas.coursesofstudy import CourseofStudyCreate, CourseofStudyRead
 from app.services import course_of_study_service
 
 router = APIRouter(prefix="/courseofstudies", tags=["coursofstudies"])
-
 
 
 @router.get("/", response_model=list[CourseofStudyRead])
@@ -18,7 +14,9 @@ def list_courses_of_study(db: Session = Depends(get_db)):
 
 @router.get("/{courseofstudy_id}", response_model=CourseofStudyRead)
 def get_course_of_study(courseofstudy_id: int, db: Session = Depends(get_db)):
-  courseofstudy = course_of_study_service.get_course_of_study(db, courseofstudy_id)
+  courseofstudy = course_of_study_service.get_course_of_study(
+    db, courseofstudy_id
+  )
   if not courseofstudy:
     raise HTTPException(status_code=404, detail="Course Of Study not found")
   return courseofstudy
@@ -26,7 +24,11 @@ def get_course_of_study(courseofstudy_id: int, db: Session = Depends(get_db)):
 
 @router.post("/", response_model=CourseofStudyRead)
 def create_course_of_study(
-  template:   CourseofStudyCreate,
- db: Session = Depends(get_db)
+  template: CourseofStudyCreate, db: Session = Depends(get_db)
 ):
   return course_of_study_service.create_course_of_study(db, template)
+
+
+@router.delete("/{cos_id}")
+def delete_courseofstudy(cos_id: int, db: Session = Depends(get_db)):
+  return course_of_study_service.delete_course_of_study(db, cos_id)
